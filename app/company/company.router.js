5a6355fec52b28490fe468ce68ea1625399477d8
Rowyda020 <rowydaramadan@gmail.com>
2024-02-26 02:58:04 +0200

    closeJob Endpoint

--- app/company/company.router.js
+++ app/company/company.router.js
@@ -2,13 +2,11 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 require("../utils/passport")(passport);
-const companyCon = require('./company.controller.js')
+const companyCon = require("./company.controller.js");
 const authGuard = passport.authenticate("cookie", { session: false });
 
-
-router.post("/createJob",authGuard,companyCon.createIntern)
-router.put("/updateJob/:jobId",authGuard,companyCon.updateIntren)
-
-
+router.post("/createJob", authGuard, companyCon.createIntern);
+router.put("/updateJob/:jobId", authGuard, companyCon.updateIntren);
+router.put("/closeJob/:jobId", authGuard, companyCon.closeIntern);
 
 module.exports = router;
